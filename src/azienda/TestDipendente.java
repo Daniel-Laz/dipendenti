814f6b870063ce13b99f562c7d70ed8d4b75220f
LAZZARONI.21130 <lazzaroni.daniel.studente@itispaleocapa.it>
2024-10-04 09:43:45 +0200

    array dipendenti

--- src/azienda/TestDipendente.java
+++ src/azienda/TestDipendente.java
@@ -2,6 +2,11 @@ package azienda;
 
 public class TestDipendente {
     public static void main(String[] args){
-        Dipendente longo_alessandro = new Dipendente("00309",1000.00,7.5);
+        Dipendente[] dipendenti = new Dipendente[2];
+        dipendenti[0]= new Dipendente("4723",1999.99,9.99);
+        dipendenti[1]= new Dipendente("4724",1499.99,7.99);
+        for (int i = 0; i< dipendenti.length; i++){
+            System.out.println(dipendenti[i].stampa());
+        }
     }
 }
