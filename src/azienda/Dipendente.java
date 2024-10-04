131916717b47c922ffd2e59ede033ef53ced38da
LAZZARONI.21130 <lazzaroni.daniel.studente@itispaleocapa.it>
2024-10-04 09:37:56 +0200

    tutti i metodi set e get

--- src/azienda/Dipendente.java
+++ src/azienda/Dipendente.java
@@ -8,12 +8,34 @@ public class Dipendente {
     public double getStipendio(){
         return stipendio;
     }
+    public double getStraordinario(){
+        return straordinario;
+    }
+    public String matricola(){
+        return matricola;
+    }
+
+    public void setMatricola(String matricola){
+        this.matricola=matricola;
+    }
+    public void setStipendio(double stipendio){
+        this.stipendio=stipendio;
+    }
+    public void setStraordinario(double straordinario){
+        this.straordinario=straordinario;
+    }
+
     public double paga(int ore){
         return stipendio + (straordinario*ore);
     }
     public String stampa(){
         return matricola + " - " + stipendio + " - " + straordinario;
     }
+
+
+
+
+
     public Dipendente(String matricola, double stipendio, double straordinario){
         this.matricola = matricola;
         this.stipendio = stipendio;
